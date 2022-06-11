c2a7cfee6287343d7f2ae01818093d9615e4e702
Hanif Maliki <hanifmaliki97@gmail.com>
2022-06-11 17:56:47 +0700

    New Look

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Homepage from './views/Homepage'
 
 function App() {
   return (
-    <div>
+    <div style={{ height: '100%' }}>
       <Homepage />
     </div>
   )
--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import PokemonLogo from '../image/pokemon.png'
+import Background from '../image/background.jpg'
+import { Button } from '@mui/material';
+
+const Container = styled.div`
+    height: 100%;
+    width: 100%;
+`
+
+const Logo = styled.img`
+    width: 40%;
+`
+
+const ButtonContainer = styled.div`
+
+`
 
 const Homepage = () => {
     return (
-        <div>
-            Hallo.
-        </div>
+        <Container style={{ backgroundImage: `url(${Background})` }}>
+            <Logo src={PokemonLogo} />
+            <ButtonContainer>
+                <Button variant='contained'>Wild Pokemon</Button>
+                <Button variant='contained'>My Pokemon</Button>
+            </ButtonContainer>
+        </Container>
     )
 }
 
